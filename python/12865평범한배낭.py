e7266e83fd6bd0144d364cfdd11f8b617845aeda
hyunsung <hyunsung9999@gmail.com>
2022-06-14 23:19:35 +0900

    평범한 배낭 주석 추가

--- python/12865평범한배낭.py
+++ python/12865평범한배낭.py
@@ -6,7 +6,7 @@ for _ in range(n): # 물건의 개수(N)만큼 입력받아야 함.
     stuff.append(list(map(int, input().split()))) # 한줄(물건 하나)씩 입력 받아서 추가.
 
 for i in range(n + 1): # stuff 배열이 [0,0]부터 시작이므로 n+1, k+1 해주어야 함. (물건은 4개긴 한데 배열이 5 * 5 배열이라서.)
-    for j in range(k + 1):
+    for j in range(k + 1): # 걍 이 반복문에서 dp 배열 채워주는거라고 생각하면 됨 !
         weight = stuff[i][0]
         value = stuff[i][1]
 
@@ -15,4 +15,4 @@ for i in range(n + 1): # stuff 배열이 [0,0]부터 시작이므로 n+1, k+1 �
         else: # 현재 물건을 넣을 수는 있지만, 기존 무게에서 무언가를 빼고 현재껄 넣을지, 현재껄 안넣고 기존상태를 유지할지는 최댓값으로 결정.
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
 
-print(dp[n][k])
+print(dp[n][k]) # 넣을 수 있는 최대 가치를 구하는거니까 당연히 dp 배열의 가장 마지막 인덱스 값이 답이 됨.
